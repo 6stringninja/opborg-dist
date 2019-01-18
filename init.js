7838a3a9f98722aeca2952444f597e8d141a4f6b
6stringninja <6294668+6stringninja@users.noreply.github.com>
2019-01-17 19:23:40 -0600

    test

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ exports.__esModule = true;
 var express = require("express");
 var app = express();
 //const config = require('../libs/configuration')
-var port = 81; //config.web.port;
+var port = 80; //config.web.port;
 var defaultRoutes = require('./web/router/default');
 app.use('/static', express.static('../web/public'));
 app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
